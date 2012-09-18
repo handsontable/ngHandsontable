638b10ba435eb39f4d97729379197939e072463d
Marcin Warpechowski <warpech@gmail.com>
2012-09-18 17:55:13 +0200

    update Handsontable, use nested object data source

--- js/split-screen.js
+++ js/split-screen.js
@@ -1,8 +1,8 @@
 function MyCtrl($scope) {
   $scope.items = [
-    {id: 9, hidden: "ddd", name: "Marcin", address: "Schellingstr. 58, Muenchen"},
-    {id: 9, hidden: "ddd", name: "Marcin", address: "Schellingstr. 58, Muenchen"},
-    {id: 9, hidden: "ddd", name: "Marcin", address: "Schellingstr. 58, Muenchen"}
+    {id: 1, name: {first: "Marcin", last: "Warpechowski"}, address: "Schellingstr. 58, Muenchen"},
+    {id: 2, name: {first: "John", last: "Irving"}, address: "Chengdu Road, Wanhua, Taipei 108"},
+    {id: 3, name: {first: "Jeremy", last: "Springsteen"}, address: "4 New York Plaza, New York, NY 10004"}
   ];
 
   $scope.dumpItems = function () {

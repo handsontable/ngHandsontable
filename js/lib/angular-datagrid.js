f48f6d05b3662170963ba5c158c8ed35f13885cb
Marcin Warpechowski <warpech@gmail.com>
2012-09-26 15:34:09 +0200

    new syntax (based on ngRepeat)

--- js/lib/angular-datagrid.js
+++ js/lib/angular-datagrid.js
@@ -2,7 +2,6 @@ angular.module('StarcounterLib', [])
   .directive('uiDatagrid', function () {
     var directiveDefinitionObject = {
       restrict: 'A',
-      require: 'ngModel',
       compile: function compile(tElement, tAttrs, transclude) {
 
         var defaultOptions = {
@@ -15,7 +14,15 @@ angular.module('StarcounterLib', [])
         var $container = $('<div class="dataTable"></div>');
 
         return function postLink(scope, element, attrs, controller) {
-          //console.log('postLink', transclude, element);
+          var expression = attrs.datarows;
+          var match = expression.match(/^\s*(.+)\s+in\s+(.*)\s*$/),
+            lhs, rhs, valueIdent, keyIdent;
+          if (!match) {
+            throw Error("Expected datarows in form of '_item_ in _collection_' but got '" +
+              expression + "'.");
+          }
+          lhs = match[1];
+          rhs = match[2];
 
           $(element).append($container);
 
@@ -27,7 +34,8 @@ angular.module('StarcounterLib', [])
 
           var i = 0;
           $(element).find('datacolumn').each(function (index) {
-            var name = $(this).attr('name');
+            var pattern = new RegExp("^(" + lhs + "\\.)");
+            var name = $(this).attr('name').replace(pattern, '');
             var title = $(this).attr('title');
             var autoCompleteProvider = $(this).attr('options');
             columns.push({data: name});
@@ -41,15 +49,8 @@ angular.module('StarcounterLib', [])
                   }
                 },
                 source: function (row, col) {
-                  var data = $container.data('handsontable').getData();
-                  var item = data[row];
-
                   var childScope = scope.$new();
-                  for(var i in item) {
-                    if(item.hasOwnProperty(i)) {
-                      childScope[i] = item[i];
-                    }
-                  }
+                  childScope.item = $container.data('handsontable').getData()[row];
                   var parsed = childScope.$eval(autoCompleteProvider);
                   return parsed;
                 }
@@ -77,25 +78,25 @@ angular.module('StarcounterLib', [])
           });
 
           /*$container.on('selectionbyprop.handsontable', function (event, r, c, r2, c2) {
-            var oldSel = scope.selectionChange;
-            var newSel = [r, c, r2, c2];
-            if (typeof oldSel === 'undefined' || oldSel[0] != newSel[0] || oldSel[1] != newSel[1] || oldSel[2] != newSel[2] || oldSel[3] != newSel[3]) {
-              scope.$apply(function () {
-                scope.selectionChange = newSel;
-              });
-            }
-          });*/
+           var oldSel = scope.selectionChange;
+           var newSel = [r, c, r2, c2];
+           if (typeof oldSel === 'undefined' || oldSel[0] != newSel[0] || oldSel[1] != newSel[1] || oldSel[2] != newSel[2] || oldSel[3] != newSel[3]) {
+           scope.$apply(function () {
+           scope.selectionChange = newSel;
+           });
+           }
+           });*/
 
           scope.$watch('dataChange', function (value) {
-            $container.handsontable("loadData", scope[attrs.ngModel]); //todo: after first iteration it is only used to rerender data
+            $container.handsontable("loadData", scope[rhs]); //todo: after first iteration it is only used to rerender data
             scope.$emit('broadcastItems');
           });
 
           /*scope.$watch('selectionChange', function (value) {
-            if (value) {
-              $container.handsontable("selectCellByProp", value[0], value[1], value[2], value[3]);
-            }
-          });*/
+           if (value) {
+           $container.handsontable("selectCellByProp", value[0], value[1], value[2], value[3]);
+           }
+           });*/
         }
       }
     };

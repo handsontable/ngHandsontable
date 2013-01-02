d9b88d6029a514f008189f72ee493d3dbbdddebc
Marcin Warpechowski <warpech@gmail.com>
2013-01-02 22:35:08 +0100

    configure ngHandsontable using object property (fix autocomplete)

--- src/angular-ui-handsontable.js
+++ src/angular-ui-handsontable.js
@@ -1,100 +1,196 @@
 angular.module('uiHandsontable', [])
-  .directive('uiHandsontable', function () {
-    var directiveDefinitionObject = {
-      restrict: 'EA',
-      compile: function compile(tElement, tAttrs, transclude) {
+  .directive('uiHandsontable', ['$compile', function ($compile) {
+  var directiveDefinitionObject = {
+    restrict: 'EA',
+    compile: function compile(tElement, tAttrs, transclude) {
 
-        var defaultSettings = {
-          columns: [],
-          colHeaders: [],
-          colWidths: [],
-          outsideClickDeselects: false,
-          autoComplete: []
-        };
+      var defaultSettings = {
+        columns: [],
+        colHeaders: true,
+        outsideClickDeselects: false,
+        autoComplete: []
+      };
 
-        var $container = $('<div class="ui-handsontable-container"></div>');
-
-        var expression = tAttrs.datarows
-          , match
-          , lhs
-          , rhs;
-        if (expression) {
-          match = expression.match(/^\s*(.+)\s+in\s+(.*)\s*$/)
-        }
+      var parseAutocomplete = function (scope, column, uiDatagrid) {
+        var expression = column.optionList
+          , match = expression.match(/^\s*(.+)\s+in\s+(.*)\s*$/);
         if (!match) {
           throw Error("Expected datarows in form of '_item_ in _collection_' but got '" +
             expression + "'.");
         }
-        lhs = match[1];
-        rhs = match[2];
-        tElement.data("uiDatagrid", {
-          lhs: lhs,
-          rhs: rhs,
-          settings: angular.extend({}, defaultSettings),
-          $container: $container
-        });
 
-        return function postLink(scope, element, attrs, controller) {
-          var uiDatagrid = element.data("uiDatagrid");
-          uiDatagrid.settings = angular.extend(uiDatagrid.settings, scope.$eval(attrs.uiHandsontable || attrs.settings));
+        var lhs = match[1]
+          , rhs = match[2]
+          , deregister
+          , deinterval
+          , childScope = scope.$new()
+          , lastItems
+          , lastQuery;
 
-          $(element).append($container);
-
-          if (typeof scope[rhs] !== 'undefined') {
-            uiDatagrid.settings['data'] = scope[rhs];
+        column.source = function (query, process) {
+          if ($.trim(query) === lastQuery) {
+            return;
           }
+          lastQuery = $.trim(query);
 
-          var pattern = new RegExp("^(" + lhs + "\\.)");
-          for (var i = 0, ilen = uiDatagrid.settings.columns.length; i < ilen; i++) {
-            uiDatagrid.settings.columns[i].data = uiDatagrid.settings.columns[i].value.replace(pattern, '');
+          if (deregister) {
+            deregister();
+            clearInterval(deinterval);
           }
-
-          $container.handsontable(uiDatagrid.settings);
-
-          $container.on('datachange.handsontable', function (event, changes, source) {
-            if (!scope.$$phase) { //if digest is not in progress
-              scope.$apply(); //programmatic change does not trigger digest in AnuglarJS so we need to trigger it automatically
-            }
-          });
-
-          $container.on('selectionbyprop.handsontable', function (event, r, p, r2, p2) {
-            scope.$emit('datagridSelection', $container, r, p, r2, p2);
-          });
-
-          // set up watchers for visible part of the table
-          scope.$watch(function () {
-            //check if visible data has changed
-            var out = ''
-              , instance = $container.data('handsontable')
-              , clen = instance.countCols();
-            for (var r = instance.rowOffset(), rlen = r + instance.countVisibleRows(); r < rlen; r++) {
-              for (var c = 0; c < clen; c++) {
-                out += instance.getDataAtCell(r, c)
-              }
-            }
-            return out;
-          }, function (newVal, oldVal) {
-            //if data has changed, render the table
-            if (newVal === oldVal) {
-              return;
-            }
-            if (scope[rhs] !== $container.handsontable('getData') && uiDatagrid.settings.columns.length > 0) {
-
-
-              $container.handsontable('updateSettings', {
-                data: scope[rhs],
-                columns: uiDatagrid.settings.columns
-              });
+          var htInstance = uiDatagrid.$container.data('handsontable');
+          var row = htInstance.getSelected()[0];
+          childScope[uiDatagrid.lhs] = scope.$eval(uiDatagrid.rhs)[row];
+          if (!column.saveOnBlur) {
+            childScope.$eval(column.value + ' = "' + $.trim(query).replace(/"/g, '\"') + '"'); //refresh value after each key stroke
+            childScope.$apply();
+          }
+          deinterval = setInterval(function () {
+            childScope.item = htInstance.getData()[row];
+            if (childScope.item) {
+              childScope.$apply();
             }
             else {
-              $container.handsontable('render', scope[rhs]); //never goes here really, fix this
+              deregister();
+              clearInterval(deinterval);
             }
-          }, false);
-        }
+          }, 100);
+          deregister = childScope.$watch(rhs, function (newVal) {
+            lastItems = newVal;
+            if (process) {
+              process(newVal);
+            }
+          }, true);
+        };
+
+        column.sorter = function (items) {
+          return items;
+        };
+
+        column.highlighter = function (item) {
+          var el;
+          var optionScope = childScope.$new();
+          optionScope[lhs] = item;
+          if (column.linker) {
+            column.linker(optionScope, function (elem) {
+              el = elem[0];
+              el.style.display = 'block';
+            });
+          }
+          else {
+            el = $compile('<span>' + column.optionTemplate + '</span>')(optionScope);
+          }
+          return el;
+        };
+
+        column.select = function () {
+          var htInstance = uiDatagrid.$container.data('handsontable');
+          if (this.$menu.find('.active').length) {
+            var index = this.$menu.find('.active').index();
+            childScope[lhs] = lastItems[index];
+            htInstance.destroyEditor();
+            childScope.$eval(column.clickrow);
+          }
+          else if (!column.strict) {
+            htInstance.destroyEditor();
+            childScope.$eval(column.value + ' = "' + $.trim(this.query).replace(/"/g, '\"') + '"'); //assign current textarea value
+          }
+          //htInstance.render();
+          $('.handsontable').each(function () {
+            $(this).handsontable('render');//render all Handsontables in the page
+          });
+
+          lastQuery = void 0;
+          return this.hide();
+        };
       }
-    };
-    return directiveDefinitionObject;
-  })
+
+      var $container = $('<div class="ui-handsontable-container"></div>');
+
+      var expression = tAttrs.datarows
+        , match
+        , lhs
+        , rhs;
+      if (expression) {
+        match = expression.match(/^\s*(.+)\s+in\s+(.*)\s*$/)
+      }
+      if (!match) {
+        throw Error("Expected datarows in form of '_item_ in _collection_' but got '" +
+          expression + "'.");
+      }
+      lhs = match[1];
+      rhs = match[2];
+      tElement.data("uiDatagrid", {
+        lhs: lhs,
+        rhs: rhs,
+        settings: angular.extend({}, defaultSettings),
+        $container: $container
+      });
+
+      return function postLink(scope, element, attrs, controller) {
+        var uiDatagrid = element.data("uiDatagrid");
+        uiDatagrid.settings = angular.extend(uiDatagrid.settings, scope.$eval(attrs.uiHandsontable || attrs.settings));
+
+        $(element).append($container);
+
+        if (typeof scope[rhs] !== 'undefined') {
+          uiDatagrid.settings['data'] = scope[rhs];
+        }
+
+        var pattern = new RegExp("^(" + lhs + "\\.)");
+        for (var i = 0, ilen = uiDatagrid.settings.columns.length; i < ilen; i++) {
+          uiDatagrid.settings.columns[i].data = uiDatagrid.settings.columns[i].value.replace(pattern, '');
+
+          if (uiDatagrid.settings.columns[i].type === 'autocomplete') {
+            parseAutocomplete(scope, uiDatagrid.settings.columns[i], uiDatagrid);
+          }
+        }
+
+        $container.handsontable(uiDatagrid.settings);
+
+        $container.on('datachange.handsontable', function (event, changes, source) {
+          if (!scope.$$phase) { //if digest is not in progress
+            scope.$apply(); //programmatic change does not trigger digest in AnuglarJS so we need to trigger it automatically
+          }
+        });
+
+        $container.on('selectionbyprop.handsontable', function (event, r, p, r2, p2) {
+          scope.$emit('datagridSelection', $container, r, p, r2, p2);
+        });
+
+        // set up watchers for visible part of the table
+        scope.$watch(function () {
+          //check if visible data has changed
+          var out = ''
+            , instance = $container.data('handsontable')
+            , clen = instance.countCols();
+          for (var r = instance.rowOffset(), rlen = r + instance.countVisibleRows(); r < rlen; r++) {
+            for (var c = 0; c < clen; c++) {
+              out += instance.getDataAtCell(r, c)
+            }
+          }
+          return out;
+        }, function (newVal, oldVal) {
+          //if data has changed, render the table
+          if (newVal === oldVal) {
+            return;
+          }
+          if (scope[rhs] !== $container.handsontable('getData') && uiDatagrid.settings.columns.length > 0) {
+
+
+            $container.handsontable('updateSettings', {
+              data: scope[rhs],
+              columns: uiDatagrid.settings.columns
+            });
+          }
+          else {
+            $container.handsontable('render', scope[rhs]); //never goes here really, fix this
+          }
+        }, false);
+      }
+    }
+  };
+  return directiveDefinitionObject;
+}])
   .directive('datacolumn', function () {
     var directiveDefinitionObject = {
       restrict: 'E',
@@ -108,7 +204,7 @@ angular.module('uiHandsontable', [])
           }
         }
 
-        tElement.data("uiDatagridAutocomplete", {
+        tElement.data("uiDatagridColumn", {
           value: tAttrs.value,
           source: null,
           saveOnBlur: ($.inArray('saveonblur', keys) !== -1), //true if element has attribute 'saveonblur'
@@ -129,16 +225,15 @@ angular.module('uiHandsontable', [])
           var column = scope.$eval(options) || {};
           column.value = attrs.value;
           column.type = type;
-
-          uiDatagrid.settings.colHeaders.push(title);
-          uiDatagrid.settings.colWidths.push(width);
+          column.title = title;
+          column.width = width;
 
           switch (type) {
             case 'autocomplete':
-              var uiDatagridAutocomplete = element.data("uiDatagridAutocomplete");
-              for (i in uiDatagridAutocomplete) {
-                if (uiDatagridAutocomplete.hasOwnProperty(i)) {
-                  column[i] = uiDatagridAutocomplete[i];
+              var uiDatagridColumn = element.data("uiDatagridColumn");
+              for (i in uiDatagridColumn) {
+                if (uiDatagridColumn.hasOwnProperty(i)) {
+                  column[i] = uiDatagridColumn[i];
                 }
               }
               break;
@@ -158,8 +253,10 @@ angular.module('uiHandsontable', [])
           }
 
           for (i in attrs) {
-            if (attrs.hasOwnProperty(i) && i.charAt(0) !== '$' && typeof column[i] === 'undefined') {
-              column[i] = childScope.$eval(attrs[i]);
+            if (attrs.hasOwnProperty(i)) {
+              if (i.charAt(0) !== '$' && typeof column[i] === 'undefined') {
+                column[i] = childScope.$eval(attrs[i]);
+              }
             }
           }
 
@@ -177,96 +274,10 @@ angular.module('uiHandsontable', [])
       compile: function compile(element, attr, linker) {
 
         return function postLink(scope, element, attrs, controller) {
-          var uiDatagridAutocomplete = element.inheritedData("uiDatagridAutocomplete");
-          var uiDatagrid = element.inheritedData("uiDatagrid");
-
-          var expression = attrs.datarows;
-          var match = expression.match(/^\s*(.+)\s+in\s+(.*)\s*$/),
-            lhs, rhs;
-          if (!match) {
-            throw Error("Expected datarows in form of '_item_ in _collection_' but got '" +
-              expression + "'.");
-          }
-          lhs = match[1];
-          rhs = match[2];
-
-          var deregister
-            , deinterval;
-
-          var childScope = scope.$new();
-
-          var lastItems;
-          var lastQuery;
-
-          uiDatagridAutocomplete.source = function (query, process) {
-            if ($.trim(query) === lastQuery) {
-              return;
-            }
-            lastQuery = $.trim(query);
-
-            if (deregister) {
-              deregister();
-              clearInterval(deinterval);
-            }
-            var row = uiDatagrid.$container.data('handsontable').getSelected()[0];
-            childScope[uiDatagrid.lhs] = scope.$eval(uiDatagrid.rhs)[row];
-            if (!uiDatagridAutocomplete.saveOnBlur) {
-              childScope.$eval(uiDatagridAutocomplete.value + ' = "' + $.trim(query).replace(/"/g, '\"') + '"'); //refresh value after each key stroke
-              childScope.$apply();
-            }
-            deinterval = setInterval(function () {
-              childScope.item = uiDatagrid.$container.data('handsontable').getData()[row];
-              if (childScope.item) {
-                childScope.$apply();
-              }
-              else {
-                deregister();
-                clearInterval(deinterval);
-              }
-            }, 100);
-            deregister = childScope.$watch(rhs, function (newVal) {
-              lastItems = newVal;
-              if (process) {
-                process(newVal);
-              }
-            }, true);
-          };
-
-          uiDatagridAutocomplete.sorter = function (items) {
-            return items;
-          };
-
-          uiDatagridAutocomplete.highlighter = function (item) {
-            var el;
-            var optionScope = childScope.$new();
-            optionScope[lhs] = item;
-            linker(optionScope, function (elem) {
-              el = elem[0];
-              el.style.display = 'block';
-            });
-            return el;
-          };
-
-          uiDatagridAutocomplete.select = function () {
-            var instance = uiDatagrid.$container.data('handsontable');
-            if (this.$menu.find('.active').length) {
-              var index = this.$menu.find('.active').index();
-              childScope[lhs] = lastItems[index];
-              instance.destroyEditor();
-              childScope.$eval(attrs.clickrow);
-            }
-            else if (!uiDatagridAutocomplete.strict) {
-              instance.destroyEditor();
-              childScope.$eval(uiDatagridAutocomplete.value + ' = "' + $.trim(this.query).replace(/"/g, '\"') + '"'); //assign current textarea value
-            }
-            //instance.render();
-            $('.handsontable').each(function () {
-              $(this).handsontable('render');//render all Handsontables in the page
-            });
-
-            lastQuery = void 0;
-            return this.hide();
-          };
+          var uiDatagridColumn = element.inheritedData("uiDatagridColumn");
+          uiDatagridColumn.optionList = attrs.datarows;
+          uiDatagridColumn.clickrow = attrs.clickrow;
+          uiDatagridColumn.linker = linker;
         }
       }
     };

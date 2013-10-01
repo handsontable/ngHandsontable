eea5f499791b9870fc19b15eba8c487f2cf46f1b
Marcin Warpechowski <warpech@gmail.com>
2013-10-01 13:34:38 +0200

    remove temporary Handsontable build

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,13 +36,13 @@ module.exports = function (grunt) {
         src: [
           'src/ie-shim.js',
           'src/angular-ui-handsontable.js',
-          'src/handsontable-tmp/jquery.handsontable.full.js'
+          'bower_components/handsontable/dist/jquery.handsontable.full.js'
         ],
         dest: 'dist/angular-ui-handsontable.full.js'
       },
       full_css: {
         src: [
-          'src/handsontable-tmp/jquery.handsontable.full.css'
+          'bower_components/handsontable/dist/jquery.handsontable.full.css'
         ],
         dest: 'dist/angular-ui-handsontable.full.css'
       }

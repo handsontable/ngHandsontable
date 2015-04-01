2f5fbf491178a1329ffbb01a51dd509cd30f5497
Will Melani <will.melani@parivedasolutions.com>
2015-04-01 10:27:38 -0700

    Update to support trivial filtering

--- dist/ngHandsontable.js
+++ dist/ngHandsontable.js
@@ -4,7 +4,7 @@
  * Copyright 2012-2014 Marcin Warpechowski
  * Licensed under the MIT license.
  * https://github.com/handsontable/ngHandsontable
- * Date: Wed Apr 01 2015 10:20:44 GMT-0700 (Pacific Daylight Time)
+ * Date: Wed Apr 01 2015 10:27:19 GMT-0700 (Pacific Daylight Time)
 */
 
 if (document.all && !document.addEventListener) { // IE 8 and lower

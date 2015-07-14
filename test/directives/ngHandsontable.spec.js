fc1e773205723abf2c63a9ca9bb121721dc640f7
Krzysztof Budnik <budnix@gmail.com>
2015-07-14 19:34:13 +0200

    tests

---
+++ test/directives/ngHandsontable.spec.js
@@ -0,0 +1,15 @@
+
+describe('MainCtrl', function() {
+  var scope;
+
+  beforeEach(angular.mock.module('ngHandsontable.directives'));
+
+  //beforeEach(angular.mock.inject(function($rootScope, $controller) {
+  //  scope = $rootScope.$new();
+  //  $controller('MainCtrl', {$scope: scope});
+  //}));
+  //
+  //it('should have variable text = "Hello World!"', function(){
+  //  expect(scope.text).toBe('Hello World!');
+  //});
+});
---
+++ test/services/settingFactory.spec.js
@@ -0,0 +1,60 @@
+
+describe('settingFactory', function() {
+
+  beforeEach(module('ngHandsontable.services'));
+
+  it('should have defined property container className', inject(function(settingFactory) {
+    expect(settingFactory.containerClassName).toBe('handsontable-container');
+  }));
+
+  it('should create new div element and instance Handsontable injected to it', inject(function(settingFactory) {
+    var element = [{appendChild: jasmine.createSpy('appendChild')}];
+    var hotSettings = {colHeaders: [1, 2], width: 200, columns: [{width: 100}]};
+    var hotInstance = {};
+    window.Handsontable = function(element, settings) {
+      hotInstance.element = element;
+      hotInstance.settings = settings;
+    };
+
+    settingFactory.initializeHandsontable(element, hotSettings);
+
+    expect(element[0].appendChild).toHaveBeenCalled();
+    expect(element[0].appendChild.calls.argsFor(0)[0].nodeName).toBe('DIV');
+    expect(hotInstance.element.nodeName).toBe('DIV');
+    expect(hotInstance.settings).toBe(hotSettings);
+  }));
+
+  it('should update Handsontable settings', inject(function(settingFactory) {
+    var hotInstance = {updateSettings: jasmine.createSpy('updateSettings')};
+    var hotSettings = {colHeaders: [1, 2], width: 200, columns: [{width: 100}]};
+
+    settingFactory.updateHandsontableSettings(null, hotSettings);
+
+    expect(hotInstance.updateSettings).not.toHaveBeenCalled();
+
+    settingFactory.updateHandsontableSettings(hotInstance, hotSettings);
+
+    expect(hotInstance.updateSettings).toHaveBeenCalledWith(hotSettings);
+  }));
+
+  it('should render Handsontable', inject(function(settingFactory) {
+    var hotInstance = {render: jasmine.createSpy('render')};
+
+    settingFactory.renderHandsontable(null);
+
+    expect(hotInstance.render).not.toHaveBeenCalled();
+
+    settingFactory.renderHandsontable(hotInstance);
+
+    expect(hotInstance.render).toHaveBeenCalled();
+  }));
+
+  it('should returns Handsontable settings from scope', inject(function(settingFactory) {
+    var hotOptions = {
+      colHeaders: [1, 2]
+    };
+
+    settingFactory.setHandsontableSettingsFromScope(hotOptions, {});
+
+  }));
+});

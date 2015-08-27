8b995f44d8909682e8d4818b93cd3e2c348b2be2
Krzysztof Budnik <budnix@gmail.com>
2015-08-27 12:46:50 +0200

    Upgrade Hot to 0.17.0

--- demo/build/app.js
+++ demo/build/app.js
@@ -253,84 +253,84 @@ require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof requ
   module.exports = {
     'intro': {
       'simple-example': {
-        url: getUrl('nudumu/2'),
+        url: getUrl('nudumu/4'),
         title: 'Simple example',
         description: 'Simple example'
       },
       'full-featured-demo': {
-        url: getUrl('xezevi/2'),
+        url: getUrl('xezevi/3'),
         title: 'Full featured demo',
         description: 'Full featured demo'
       }
     },
     'configuration': {
       'configuration-by-object': {
-        url: getUrl('getazu/2'),
+        url: getUrl('getazu/3'),
         title: 'By `settings` object',
         description: 'Configuration by setting `settings` object'
       },
       'configuration-declarative-way': {
-        url: getUrl('jupeme/2'),
+        url: getUrl('jupeme/3'),
         title: 'In declarative way',
         description: 'Configuration in declarative way'
       }
     },
     'columns': {
       'add-remove-column-ng-repeat': {
-        url: getUrl('muluto/2'),
+        url: getUrl('muluto/4'),
         title: 'Add/Remove (ng-repeat)',
         description: 'Add/Remove columns using ng-repeat'
       },
       'add-remove-column-by-attr': {
-        url: getUrl('xayeru/1'),
+        url: getUrl('xayeru/3'),
         title: 'Add/Remove (`columns` attribute)',
         description: 'Add/Remove columns using `columns` attribute'
       }
     },
     'binding': {
       'data-binding': {
-        url: getUrl('lupile/3'),
+        url: getUrl('lupile/4'),
         title: 'Data binding',
         description: 'Data binding'
       },
       'settings-binding': {
-        url: getUrl('xaqasi/2'),
+        url: getUrl('xaqasi/3'),
         title: 'Table settings binding',
         description: 'Table settings binding'
       }
     },
     'callbacks': {
       'callbacks-by-object': {
-        url: getUrl('nayito/4', 'html,js,console,output'),
+        url: getUrl('nayito/5', 'html,js,console,output'),
         title: 'By `settings` object',
         description: 'Listening callbacks using `settings` object'
       },
       'callbacks-declarative-way': {
-        url: getUrl('pucale/2', 'html,js,console,output'),
+        url: getUrl('pucale/4', 'html,js,console,output'),
         title: 'In declarative way',
         description: 'Listening callbacks in declarative way'
       }
     },
     'plugins': {
       'copy-paste-context-menu': {
-        url: getUrl('zohoge/1'),
+        url: getUrl('zohoge/2'),
         title: 'Enable copy/paste in context menu',
         description: 'Enable copy/paste in context menu'
       }
     },
     'other': {
       'access-to-instance': {
-        url: getUrl('fovoxu/2'),
+        url: getUrl('fovoxu/4'),
         title: 'Access to Handsontable instance',
         description: 'Access to Handsontable instance'
       },
       'custom-validator': {
-        url: getUrl('qoweju/1'),
+        url: getUrl('qoweju/3'),
         title: 'Custom validator',
         description: 'Custom validator'
       },
       'custom-renderer': {
-        url: getUrl('locome/1'),
+        url: getUrl('locome/2'),
         title: 'Custom renderer',
         description: 'Custom renderer'
       }
